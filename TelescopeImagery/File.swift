a044f539553303b0c26b7e1448ec8c1d0c34bfa6
rubrdcky <jwarren4881@gmail.com>
2017-05-13 11:24:23 -0400

    began implementing mgpColor Collection view

---
+++ TelescopeImagery/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  TelescopeImagery
+//
+//  Created by Jacob Warren on 5/13/17.
+//  Copyright © 2017 Jacob Warren. All rights reserved.
+//
+
+import Foundation
---
+++ TelescopeImagery/TelescopeImagery/mgpCell.swift
@@ -0,0 +1,16 @@
+//
+//  mgpCell.swift
+//  TelescopeImagery
+//
+//  Created by Jacob Warren on 5/13/17.
+//  Copyright © 2017 Jacob Warren. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class mgpCell: UICollectionViewCell{
+    
+    @IBOutlet weak var mgpColorLabel: UILabel!
+    
+}
