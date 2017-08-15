90b050abfbb5d71ee7ca1c0b0e7a6583893da125
rubrdcky <jwarren4881@gmail.com>
2017-08-15 14:29:18 -0400

    rpmCalcView pretty much ready

--- Mill Mate/Mill Mate/RPMCalcView.swift
+++ Mill Mate/Mill Mate/RPMCalcView.swift
@@ -12,16 +12,58 @@ import UIKit
 class RPMCalcView: UIViewController
 {
     
-    var toolDiameter: Float!
-    var sfm: Float!
+    var toolDiaNum: Float!
+    var sfmNum: Float!
+    var storedRPM: Float!
+    var rpmResultsSegue = "rpmResultSegue"
     
     @IBOutlet weak var rpmCalculate: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
     
     @IBOutlet weak var toolDiameterField: UITextField!
     
     @IBOutlet weak var sfmField: UITextField!
     
     @IBAction func calculateButtomPressed(_ sender: Any) {
+        
+        if (self.toolDiameterField.text == "" || self.sfmField.text == "")
+        {
+            storedRPM = 0
+        }else{
+            
+            toolDiaNum = Float(toolDiameterField.text!)!
+            sfmNum = Float(sfmField.text!)!
+            storedRPM = (sfmNum * (12/Float.pi))/toolDiaNum
+        }
+        
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        errorLabel.text = ""
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
+        if let ident = identifier{
+            if ident == rpmResultsSegue{
+                if storedRPM == 0{
+                    self.errorLabel.text = "You seem to be missing something"
+                return false
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == rpmResultsSegue{
+            let viewController = segue.destination as! RPMResultsView
+            viewController.rpmPassed = String(storedRPM)
+        }
+    }
+    
+    
     
 }
---
+++ Mill Mate/Mill Mate/RPMResultsView.swift
@@ -0,0 +1,17 @@
+//
+//  RPMResultsView.swift
+//  Mill Mate
+//
+//  Created by Jacob Warren on 8/15/17.
+//  Copyright © 2017 Jacob Warren. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RPMResultsView: UIViewController
+{
+    var rpmCalc = RPMResultsView()
+    var rpmPassed:String!
+    
+}
